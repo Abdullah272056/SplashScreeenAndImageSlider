1690a6ce26aac09d2f7fb27a120cef86adae4d76
Abdullah272056 <abdullah272056@gmail.com>
2021-04-01 21:23:16 +0600

    add design in activity_splash_screen.xml

--- app/src/main/java/com/example/splashscreeenandimageslider/SplashScreen.java
+++ app/src/main/java/com/example/splashscreeenandimageslider/SplashScreen.java
@@ -1,7 +1,6 @@
 package com.example.splashscreeenandimageslider;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
