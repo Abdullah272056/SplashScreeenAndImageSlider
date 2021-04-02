2b79e00a1336acef27a4557a19865bb5370c9861
Abdullah272056 <abdullah272056@gmail.com>
2021-04-02 15:31:21 +0600

    view finding and image set in flipper

--- app/src/main/java/com/example/splashscreeenandimageslider/MainActivity.java
+++ app/src/main/java/com/example/splashscreeenandimageslider/MainActivity.java
@@ -20,9 +20,39 @@ int value=0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //
+        flipper=findViewById(R.id.viewFlipperId);
+        button=findViewById(R.id.buttonId);
+        textView=findViewById(R.id.textViewId);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                value++;
+                if (value==10){
+                    Intent intent=new Intent(MainActivity.this,SplashScreen.class);
+                     startActivity(intent);
+                }
+                textView.setText(String.valueOf(value));
+
+            }
+        });
+        int imageArray[]={R.drawable.download,R.drawable.download1,R.drawable.download2,R.drawable.images,
+                R.drawable.images1,R.drawable.images2,R.drawable.images3,R.drawable.images4,R.drawable.images5};
+
+for (int i=0;i<imageArray.length;i++){
+    showImage(imageArray[i]);
+}
 
     }
 
-  
+    public void showImage(int image){
+        ImageView imageView=new ImageView(this);
+        imageView.setBackgroundResource(image);
+        flipper.addView(imageView);
+        flipper.setAutoStart(true);
+        flipper.setFlipInterval(2000);
+        //flipper.setAnimation();
+        flipper.setInAnimation(this,android.R.anim.slide_in_left);
+        flipper.setOutAnimation(this,android.R.anim.slide_out_right);
+    }
 }
